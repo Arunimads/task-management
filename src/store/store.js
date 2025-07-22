1c343512219561d2eb6178efff722ca356efef55
Arunima Ds <dsarunima7@gmail.com>
2025-07-22 22:34:06 +0530

    added themeSlice to store darkmode state

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import authReducer from "./slices/authSlice";
 import taskReducer from "./slices/taskSlice";
+import themeReducer from "./slices/themeSlice";
 
 const authPersistConfig = {
   key: "auth",
@@ -10,13 +11,19 @@ const authPersistConfig = {
   whitelist: ["token", "user"],
 };
 
+const themePersistConfig = {
+  key: "theme",
+  storage,
+};
 
 const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+const persistedThemeReducer = persistReducer(themePersistConfig, themeReducer);
 
 export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
     tasks: taskReducer,
+    theme: persistedThemeReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
