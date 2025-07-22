e6b36ab9b16d1020d989c2ee72ed5ad0c9f9d320
Arunima Ds <dsarunima7@gmail.com>
2025-07-22 22:24:51 +0530

    created dashboard and dialog components

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { Container, Button, Spinner, Alert } from "react-bootstrap";
+
+const Dashboard = () => {
+  return (
+    <Container className="py-4">
+      <div className="d-flex justify-content-between align-items-center mb-4">
+        <h1>Task Management Dashboard</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default Dashboard;
